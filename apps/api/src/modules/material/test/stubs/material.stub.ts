1be9bf06974e271f3e465f61b04e19d6abab7b9a
Amir Khanjani <amirh.khanjani@gmail.com>
2022-05-29 19:16:55 +0430

    changed stub data

--- apps/api/src/modules/material/test/stubs/material.stub.ts
+++ apps/api/src/modules/material/test/stubs/material.stub.ts
@@ -1,9 +1,9 @@
 import { Material } from '@fm/nest/material/interface';
 
 export const materialStub = (): Material => ({
-  id: '6293745c1d4f29759d32c4b4',
-  createdAt: new Date('2022-05-29T13:26:38.293Z'),
-  category: ['6293748e711b2a6a9afc1d1f', '6293748e711b2a6a9afc1d20'],
+  id: '6293745c1d4f29749d32c4b4',
+  createdAt: new Date('2012-01-29T13:26:38.293Z'),
+  category: ['6293748e711b2a6a9afk1d1f', '6293778e711b2a6b9afc1d20'],
   formData: 'form-data',
   title: 'title',
   type: 'type',
