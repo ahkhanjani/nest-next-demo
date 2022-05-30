f57d8963a7fa9047f007ffd30403f3406ce46949
Amir Khanjani <amirh.khanjani@gmail.com>
2022-05-30 14:59:58 +0430

    removed plugin

--- apps/api/src/modules/user/users.module.ts
+++ apps/api/src/modules/user/users.module.ts
@@ -2,22 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
-import { UserSchema } from '@fm/nest/user/model';
+import { UserSchema } from './schema/user.schema';
 import { IsUserAlreadyExistingConstraint } from '@fm/nest/user/validator';
+import { User } from '@fm/nest/user/interface';
 
 @Module({
   imports: [
     MongooseModule.forFeatureAsync([
       {
-        name: 'users',
-        useFactory: () => {
-          const schema = UserSchema;
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          schema.plugin(require('mongoose-unique-validator'), {
-            message: 'already taken.',
-          });
-          return schema;
-        },
+        name: User.name,
+        useFactory: () => UserSchema,
       },
     ]),
   ],
