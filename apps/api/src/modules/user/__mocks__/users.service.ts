b23546cf88d76e7cafcf33c5be0c355a7a3a4683
Amir Khanjani <amirh.khanjani@gmail.com>
2022-05-30 15:01:38 +0430

    added users test

---
+++ apps/api/src/modules/user/__mocks__/users.service.ts
@@ -0,0 +1,16 @@
+import { User } from '@fm/nest/user/interface';
+import { MockType } from '@fm/types';
+import { UsersService as ServiceType } from '../users.service';
+import { userStub } from '../test/stubs/user.stub';
+import { UserResponse } from '@fm/nest/user/dto';
+
+export const UsersService = jest.fn(
+  (): MockType<ServiceType> => ({
+    count: jest.fn((users: User[]): number => users.length),
+    findOne: jest.fn(
+      (id: string): User => (id === userStub().id ? userStub() : undefined)
+    ),
+    findAll: jest.fn((): User[] => [userStub()]),
+    createOne: jest.fn((): UserResponse => ({ user: userStub() })),
+  })
+);
---
+++ apps/api/src/modules/user/test/users.resolver.spec.ts
@@ -0,0 +1,136 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users.service';
+import { UsersResolver } from '../users.resolver';
+import { userStub } from './stubs/user.stub';
+import { User } from '@fm/nest/user/interface';
+
+jest.mock('../users.service.ts');
+
+describe('UsersResolver', () => {
+  let resolver: UsersResolver;
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersResolver, UsersService],
+    }).compile();
+
+    resolver = module.get<UsersResolver>(UsersResolver);
+    service = module.get<UsersService>(UsersService);
+    jest.clearAllMocks();
+  });
+
+  describe('usersResolver', () => {
+    it('should be defined', () => {
+      expect(resolver).toBeDefined();
+    });
+  });
+
+  describe('usersService', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+  });
+
+  //
+  // ─── QUERY ──────────────────────────────────────────────────────────────────────
+  //
+
+  describe('getUser', () => {
+    describe('when getUser is called', () => {
+      describe('if user exists', () => {
+        let user: User;
+
+        beforeEach(async () => {
+          user = await resolver.getUser(userStub().id);
+        });
+
+        it('should call usersService', () => {
+          expect(service.findOne).toHaveBeenCalledWith(userStub().id);
+          expect(service.findOne).toHaveBeenCalledTimes(1);
+        });
+
+        it('should return the user', () => {
+          expect(user).toEqual<User>(userStub());
+        });
+      });
+
+      describe('if user does not exist', () => {
+        let user: User;
+
+        beforeEach(async () => {
+          user = await resolver.getUser('someId');
+        });
+
+        it('should call usersService', () => {
+          expect(service.findOne).toHaveBeenCalledWith('someId');
+          expect(service.findOne).toHaveBeenCalledTimes(1);
+        });
+
+        it('should return undefined', () => {
+          expect(user).toBeUndefined();
+        });
+      });
+    });
+  });
+
+  describe('getUsers', () => {
+    describe('when getUsers is called', () => {
+      let users: User[];
+
+      beforeEach(async () => {
+        users = await resolver.getUsers();
+      });
+
+      it('should call usersService', () => {
+        expect(service.findAll).toHaveBeenCalled();
+        expect(service.findAll).toHaveBeenCalledTimes(1);
+      });
+
+      it('should return users', () => {
+        expect(users).toEqual<User[]>([userStub()]);
+      });
+    });
+  });
+
+  //
+  // ─── MUTATION ───────────────────────────────────────────────────────────────────
+  //
+
+  //   describe('createUsers', () => {
+  //     describe('when createUsers is called', () => {
+  //       let response: CreateUsersResponse;
+  //       let createUsersDto: CreateUsersInput;
+  //
+  //       beforeEach(async () => {
+  //         // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //         const { id: _, createdAt: __, category, ...rest } = userStub();
+  //
+  //         createUsersDto = {
+  //           category,
+  //           userDataArray: [rest, rest],
+  //         };
+  //
+  //         response = await resolver.createUsers(createUsersDto);
+  //       });
+  //
+  //       it('should call usersService', () => {
+  //         expect(service.createMany).toHaveBeenCalledWith(createUsersDto);
+  //         expect(service.createMany).toHaveBeenCalledTimes(1);
+  //       });
+  //
+  //       it('should return response', () => {
+  //         expect(response).toEqual<CreateUsersResponse>({
+  //           createdUsers: [
+  //             {
+  //               createdUser: userStub(),
+  //             },
+  //             {
+  //               createdUser: userStub(),
+  //             },
+  //           ],
+  //         });
+  //       });
+  //     });
+  //   });
+});
