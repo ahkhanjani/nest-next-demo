9b20aa4e690b0f6f2972fbca405738f12a2ec64e
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-06-25 11:00:33 +0430

    added libs

--- apps/material-web/src/page-containers/WizardPage/steps/MaterialCreateStep/MaterialForm/FormSchemaProvider.tsx
+++ apps/material-web/src/page-containers/WizardPage/steps/MaterialCreateStep/MaterialForm/FormSchemaProvider.tsx
@@ -5,6 +5,18 @@ const FormSchemaContext = createContext();
 const FormSchemaProvider: React.FC<PropsWithChildren<unknown>> = ({
   children,
 }) => {
+  await Promise.all(
+    files.map(async (file) => {
+      try {
+
+
+        createdSchemas.push(createdMaterialFormSchema);
+      } catch (error) {
+        errorFileNames.push(file.filename);
+      }
+    })
+  );
+
   return (
     <FormSchemaContext.Provider value={}>{children}</FormSchemaContext.Provider>
   );
