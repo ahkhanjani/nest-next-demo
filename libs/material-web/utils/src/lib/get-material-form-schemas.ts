8194455daf3879c3d1a930e8ed80c8a2c2c0640d
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-06-26 12:43:42 +0430

    cleanup

--- libs/material-web/utils/src/lib/get-material-form-schemas.ts
+++ libs/material-web/utils/src/lib/get-material-form-schemas.ts
@@ -1,32 +1,38 @@
-import { parse } from 'yaml';
+import { parse as parseYaml } from 'yaml';
 import * as fs from 'fs';
 // types
 import type { JSONSchema7 } from 'json-schema';
 import type { MaterialFormSchema } from '@fm/material-web/types';
 
-const dirname = 'libs/material-web/assets/src/material-form-schema';
+/** Directory where the schema files are located. */
+const DIRNAME = 'libs/material-web/assets/src/material-form-schema';
 
-export async function getMaterialFormSchemas(): Promise<MaterialFormSchema[]> {
-  const filenames: string[] = fs.readdirSync(dirname);
+/**
+ * Parses and processes the YAML material form schema files.
+ * @returns {MaterialFormSchema[]} Processed material form schemas.
+ */
+export function getMaterialFormSchemas(): MaterialFormSchema[] {
+  // get file names
+  const filenames: string[] = fs.readdirSync(DIRNAME);
 
-  const materialFormSchemas: MaterialFormSchema[] = await Promise.all(
-    filenames.map(async (filename) => {
-      const strYamlSchema = fs.readFileSync(`${dirname}/${filename}`, 'utf-8');
-      const parsedSchema = await parseFormSchema(strYamlSchema);
-      return parsedSchema;
-    })
+  // process files
+  const materialFormSchemas: MaterialFormSchema[] = filenames.map(
+    (filename) => {
+      // read yaml file content
+      const strYamlSchema = fs.readFileSync(`${DIRNAME}/${filename}`, 'utf-8');
+
+      // parse yaml form schema
+      const parsedSchema: JSONSchema7 = parseYaml(strYamlSchema);
+
+      // process the parsed schema
+      const processedSchema: MaterialFormSchema = {
+        title: parsedSchema.title,
+        schema: parsedSchema,
+      };
+
+      return processedSchema;
+    }
   );
+
   return materialFormSchemas;
-}
-
-async function parseFormSchema(
-  strYamlSchema: string
-): Promise<MaterialFormSchema> {
-  const jsonSchema: JSONSchema7 = parse(strYamlSchema);
-
-  const parsedSchema: MaterialFormSchema = {
-    title: jsonSchema.title,
-    schema: jsonSchema,
-  };
-  return parsedSchema;
 }
