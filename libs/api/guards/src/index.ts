56badde6f99044c29f491f3bba437ebc0de323ad
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-06-27 08:21:02 +0430

    created material status nest guard

---
+++ libs/api/guards/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/material-status/decorators/material-status.decorator';
+export * from './lib/material-status/enums/material-status.enum';
---
+++ libs/api/guards/src/lib/material-status/enums/material-status.enum.ts
@@ -0,0 +1,4 @@
+export enum MaterialStatus {
+  Published = 'published',
+  Draft = 'draft',
+}
