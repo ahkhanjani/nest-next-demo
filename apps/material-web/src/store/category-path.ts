ccb8fb4416108b329223b1c4ba665a2795e654cf
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-06-27 08:02:19 +0430

    fixed import

--- apps/material-web/src/store/category-path.ts
+++ apps/material-web/src/store/category-path.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 // types
-import type { PathCategory } from '@fm/shared-interfaces';
+import type { PathCategory } from '@fm/types';
 
 const initialState: State = { path: [], endId: '' };
 
