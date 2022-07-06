0e41cda6c6e083e321c7736edea083c99431c623
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-07-06 16:00:12 +0430

    added nextjs default config

--- apps/main-web/postcss.config.js
+++ apps/main-web/postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production'
