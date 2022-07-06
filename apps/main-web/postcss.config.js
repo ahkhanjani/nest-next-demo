e92872c107b689112c2afb282f5bf124b1d7d7f0
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-07-06 15:57:45 +0430

    optimized config

--- apps/main-web/postcss.config.js
+++ apps/main-web/postcss.config.js
@@ -1,10 +1,11 @@
-const { join } = require('path');
-
 module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
-    tailwindcss: { config: join(__dirname, 'tailwind.config.js') },
+    tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? { cssnano: { preset: 'advanced' } }
+      : {}),
   },
 };
--- apps/main-web/tailwind.config.js
+++ apps/main-web/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./libs/**/*.{tsx,}', './apps/**/*.{tsx,}'],
+  content: ['**/*.{jsx,tsx}', ...createGlobPatternsForDependencies(__dirname)],
   theme: {
     extend: {
       fontFamily: {
@@ -11,17 +12,18 @@ module.exports = {
       },
     },
   },
+  prefix: 'tw-', // note: use tailwind styles like "tw-*"
   plugins: [require('daisyui')],
 
   // See https://daisyui.com/docs/config/
   daisyui: {
-    styled: true, // See https://daisyui.com/docs/config/#styled
-    themes: ['light', 'dark'], // See https://daisyui.com/docs/config/#themes
-    base: true, // See https://daisyui.com/docs/config/#base
-    utils: true, // See https://daisyui.com/docs/config/#utils
-    logs: true, // See https://daisyui.com/docs/config/#logs
-    rtl: false, // See https://daisyui.com/docs/config/#rtl
-    prefix: 'daisy-', // See https://daisyui.com/docs/config/#prefix
-    darkTheme: 'dark', // See https://daisyui.com/docs/config/#darktheme
+    styled: true,
+    themes: ['light', 'dark'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: 'daisy-', // note: use daisyui styles like "tw-daisy-*"
+    darkTheme: 'dark',
   },
 };
