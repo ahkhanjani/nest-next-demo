69048c9da4d855b6c7b8278ee4b21f394ffda791
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-07-09 03:13:39 +0430

    added device select components (for testing)

--- apps/main-web/src/page-containers/ClassroomPage/index.tsx
+++ apps/main-web/src/page-containers/ClassroomPage/index.tsx
@@ -1,5 +1,16 @@
+import { MediaDeviceProvider, DeviceSelectList } from '@fm/daily.co';
+import { RecoilRoot } from 'recoil';
+
 const ClassroomPageContainer: React.FC<ClassroomPageProps> = ({ roomId }) => {
-  return <></>;
+  return (
+    <>
+      <RecoilRoot>
+        <MediaDeviceProvider>
+          <DeviceSelectList />
+        </MediaDeviceProvider>
+      </RecoilRoot>
+    </>
+  );
 };
 export default ClassroomPageContainer;
 
