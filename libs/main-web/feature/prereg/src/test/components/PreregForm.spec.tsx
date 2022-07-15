d26bff1aa4eb22b9368743b9f63d526afa856744
Amir <72540492+ahkhanjani@users.noreply.github.com>
2022-07-15 15:58:06 +0430

    fixed a name

--- libs/main-web/feature/prereg/src/test/components/PreregForm.spec.tsx
+++ libs/main-web/feature/prereg/src/test/components/PreregForm.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 // cmp
 import PreregForm from '../../lib/components/PreregForm';
 
-describe('preregForm', () => {
+describe('PreregForm', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<PreregForm />);
     expect(baseElement).toBeTruthy();
