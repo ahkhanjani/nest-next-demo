12fb8988445c7f6c91b5345197042b63091a8a7c
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-07-16 21:24:56 +0430

    fixed lib name

--- libs/main-web/daily-co/jest.config.js
+++ libs/main-web/daily-co/jest.config.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 module.exports = {
-  displayName: 'main-web-daily.co',
+  displayName: 'main-web-daily-co',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/main-web/daily.co',
+  coverageDirectory: '../../../coverage/libs/main-web/daily-co',
 };
