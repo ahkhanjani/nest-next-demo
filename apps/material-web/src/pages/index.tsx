9cfe5c1817de0aedd3d3f0edd837c8d20c6fb1f2
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-07-28 11:52:40 +0430

    feat: redirect to browse on load

--- apps/material-web/src/pages/index.tsx
+++ apps/material-web/src/pages/index.tsx
@@ -1,6 +1,18 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const IndexPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('./browse');
+
+    return () => {
+      console.log('Redirecting...');
+    };
+  }, [router]);
+
   return undefined;
 };
 export default IndexPage;
