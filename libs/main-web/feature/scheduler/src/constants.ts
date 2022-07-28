625ef375893f97e943ce020df90830cd68118b34
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-07-29 00:09:25 +0430

    fixed constants

--- libs/main-web/feature/scheduler/src/constants.ts
+++ libs/main-web/feature/scheduler/src/constants.ts
@@ -1,20 +1,18 @@
-import type { TimeRange } from './types/time-range';
+import type { Hour, TimeRange } from './types/time-range';
 
-const hours = [
-  '12',
-  ...Array.from({ length: 11 }, (_, i) => (i + 1).toString()),
-];
-const mins = ['00', '15', '30', '45'];
 const periods = ['AM', 'PM'];
+const hours = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
+const mins = ['00', '15', '30', '45'];
 
-export const hourList: string[] = [];
+const hourArr: Hour[] = [];
 periods.forEach((p) => {
   hours.forEach((h) => {
     mins.forEach((m) => {
-      hourList.push(`${h}:${m} ${p}`);
+      hourArr.push(`${h}:${m} ${p}` as Hour);
     });
   });
 });
+export const hourList = Object.freeze(hourArr);
 
 const defaultRange: TimeRange = { from: '10:00 AM', to: '8:00 PM' };
 /** Call this to get a fresh default range each time. This is to prevent referencing the object. */
