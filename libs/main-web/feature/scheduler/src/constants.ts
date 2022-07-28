3b7d8fbd21bf60574bf430c705f0995d134ba228
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-07-28 23:56:23 +0430

    added comment

--- libs/main-web/feature/scheduler/src/constants.ts
+++ libs/main-web/feature/scheduler/src/constants.ts
@@ -17,5 +17,5 @@ periods.forEach((p) => {
 });
 
 const defaultRange: TimeRange = { from: '10:00 AM', to: '8:00 PM' };
-
+/** Call this to get a fresh default range each time. This is to prevent referencing the object. */
 export const getDefaultRange = () => ({ ...defaultRange });
