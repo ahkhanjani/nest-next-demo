7a524d5f287f656c48b03044faf725fa94b9dc49
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-07-29 15:41:09 +0430

    added base elements

--- libs/main-web/feature/scheduler/src/features/teacher-calendar/blockout-dates/BlockoutDates.tsx
+++ libs/main-web/feature/scheduler/src/features/teacher-calendar/blockout-dates/BlockoutDates.tsx
@@ -1,4 +1,17 @@
+import { PlusIcon } from '@heroicons/react/outline';
+
 const BlockoutDates: React.FC = () => {
-  return <div>BlockoutDates</div>;
+  return (
+    <>
+      <p>
+        Add days when you do not want to get bookings or availability changes
+        from your weekly days
+      </p>
+      <button className="tw-daisy-btn tw-gap-2">
+        Add blockout dates
+        <PlusIcon className="tw-w-2 tw-h-2" />
+      </button>
+    </>
+  );
 };
 export default BlockoutDates;
