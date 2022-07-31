81dc3681be3f1fd52d744658a65620bc4b114bdc
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-08-01 04:24:59 +0430

    .

--- apps/api/src/decorators/sanitize-html.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators } from '@nestjs/common';
-import { Transform } from 'class-transformer';
-import * as sanitizeHtml from 'sanitize-html';
-
-export const SanitizeHtml = (input: string) =>
-  applyDecorators(Transform(() => sanitizeHtml(input)));
