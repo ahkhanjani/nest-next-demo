78f00914a2b8833afdc93a472c204625af871000
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2022-08-04 15:59:25 +0430

    fix tests

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,22 +1,16 @@
 import { Module } from '@nestjs/common';
-// modules
 import { PassportModule } from '@nestjs/passport';
+
+import { LocalStrategy } from './strategy';
 import { UsersModule } from '../user/users.module';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { AuthController } from './auth.controller';
-import { LocalStrategy } from './strategy';
 
 @Module({
   imports: [UsersModule, PassportModule.register({ session: true })],
-  providers: [
-    AuthService,
-    LocalStrategy,
-    AuthResolver,
-    AuthController,
-    // FacebookStrategy,
-    // GoogleStrategy,
-  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, AuthResolver],
   exports: [AuthService],
 })
 export class AuthModule {}
